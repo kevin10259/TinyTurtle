4cc10a83d2f9aa52e639dce83317d39b342d86ab
Kevin Moposita <kmoposita@aclanyc.com>
2017-03-22 12:33:33 +0000

    New Shape

--- script.js
+++ script.js
@@ -1,19 +1,52 @@
 TinyTurtle.apply(window);
 
-forward(40);
-right(65);
-forward(40);
-right(45);
-forward(45);
-right(65);
-forward(42);
-right(95);
-forward(210);
-right(180);
-forward(270)
-left(180);
-forward(60);
-right(87);
-forward(42);
-left(88);
-forward(79);
+// forward(40);
+// right(65);
+// forward(40);
+// right(45);
+// forward(45);
+// right(65);
+// forward(42);
+// right(95);
+// forward(210);
+// right(180);
+// forward(270)
+// left(180);
+// forward(60);
+// right(87);
+// forward(42);
+// left(88);
+// forward(79);
+// stamp();
+
+
+function square (x) {
+    forward(x);
+    right(90);
+    forward(x);
+    right(90);
+    forward(x);
+    right(90);
+    forward(x);
+    right(90);
+    forward(x);
+}
+
+// square(20);
+
+function hexagon (x) {
+    left(70);
+    forward(x);
+    right(70);
+    forward(x);
+    right(70);
+    forward(x);
+    right(40);
+    forward(x);
+    right(70);
+    forward(x);
+    right(70);
+    forward(x);
+}
+
+hexagon(25);
